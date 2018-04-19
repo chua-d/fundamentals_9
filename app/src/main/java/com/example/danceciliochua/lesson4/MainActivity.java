60bf52804a5c3485cf52513e9bfc98dd0d4f076e
Dan Cecilio Chua <chua-d@cyscorpions.com>
2018-04-19 09:43:27 +0800

    Add Project

---
+++ app/src/main/java/com/example/danceciliochua/lesson4/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.danceciliochua.lesson4;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void showFoodOrder(String message) {
+        displayToast(message);
+        Intent intent = new Intent(this, OrderActivity.class);
+        startActivity(intent);
+    }
+
+    public void showDonutOrder(View view) {
+        showFoodOrder(getString(R.string.donut_order_message));
+    }
+
+    public void showIceCreamOrder(View view) {
+        showFoodOrder(getString(R.string.ice_cream_order_message));
+    }
+
+    public void showFroyoOrder(View view) {
+        showFoodOrder(getString(R.string.froyo_ordered_message));
+    }
+}
