f8bb3de61d8e85a04b2d82d8e03f543557236242
Dan Cecilio Chua <chua-d@cyscorpions.com>
2018-04-19 10:45:46 +0800

    Finish Lesson 9.2

--- app/src/main/java/com/example/danceciliochua/lesson4/MainActivity.java
+++ app/src/main/java/com/example/danceciliochua/lesson4/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.danceciliochua.lesson4;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -15,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
 
 
 
@@ -22,10 +30,24 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                displayMap();
             }
         });
+
+        PreferenceManager.setDefaultValues(this, R.xml.pref_general, false);
+        PreferenceManager.setDefaultValues(this, R.xml.pref_notification, false);
+        PreferenceManager.setDefaultValues(this, R.xml.pref_data_sync, false);
+
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        String marketPref = sharedPref.getString("sync_frequency", "-1");
+        Toast.makeText(this, marketPref, Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
 
     public void displayToast(String message) {
@@ -50,4 +72,53 @@ public class MainActivity extends AppCompatActivity {
     public void showFroyoOrder(View view) {
         showFoodOrder(getString(R.string.froyo_ordered_message));
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_order:
+                displayToast(getString(R.string.action_order_message));
+
+                return true;
+
+            case R.id.action_status:
+                displayToast(getString(R.string.action_status_message));
+
+                return true;
+
+            case R.id.action_favorites:
+                displayToast(getString(R.string.action_favorites_message));
+
+                return true;
+
+            case R.id.action_contact:
+                displayToast(getString(R.string.action_contact_message));
+
+                return true;
+
+            case R.id.action_settings:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+
+                return true;
+
+            default:
+
+        }
+
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    public void displayMap() {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+
+        String data = getString(R.string.google_mtv_coord_zoom12);
+        intent.setData(Uri.parse(data));
+        if(intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
 }
